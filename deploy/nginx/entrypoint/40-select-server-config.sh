e7ce650b826c0fc435e79c60aa2a48785162316e
eashhyeon3232 <jihyeon-lou3232@naver.com>
2026-08-18 09:40:08 +0900

    fix(global): staging Nginx TLS 배포 검증

--- deploy/nginx/entrypoint/40-select-server-config.sh
+++ deploy/nginx/entrypoint/40-select-server-config.sh
@@ -2,10 +2,11 @@
 set -eu
 
 certificate="/etc/letsencrypt/live/${STAGING_DOMAIN}/fullchain.pem"
+private_key="/etc/letsencrypt/live/${STAGING_DOMAIN}/privkey.pem"
 
 if [ "${MIRIYUM_STAGING_FORCE_HTTP:-false}" = "true" ]; then
     template="/opt/miriyum-nginx-templates/http.conf.template"
-elif [ -r "${certificate}" ]; then
+elif [ -r "${certificate}" ] && [ -r "${private_key}" ]; then
     template="/opt/miriyum-nginx-templates/https.conf.template"
 else
     template="/opt/miriyum-nginx-templates/http.conf.template"
